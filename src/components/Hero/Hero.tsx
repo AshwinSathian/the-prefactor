49ecd3bffdc2c2b51fdaf147af1140dc430a7233
Ashwin Sathian <ashwinsathyan19@gmail.com>
2023-11-28 17:17:08 +0530

    Added Video backdrop for Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,33 +6,38 @@ import { Button } from "primereact/button";
 
 const HeroSection: React.FC = () => {
   return (
-    <div
-      className={`${styles.heroSection} bg-primary-color flex flex-col justify-evenly`}
-    >
-      <img
-        src="/assets/logo-2.png"
-        alt="The Pro-Factor"
-        className="w-60 mx-auto"
-      />
-      <div className="w-full flex flex-col items-center justify-center p-10 gap-4">
-        <div
-          className={`${styles.heroHeadline} flex flex-col gap-1 p-3 text-7xl text-center`}
-        >
-          <div>We're all</div>
-          <div>SET to...</div>
-          <div className="text-xl">Spark. Elevate. Transform!</div>
+    <div className={styles.heroSection}>
+      <video autoPlay loop muted className={styles.backgroundVideo}>
+        <source src="./assets/hero-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className={styles.overlay}>
+        <img
+          src="/assets/logo-2.png"
+          alt="The Pro-Factor"
+          className="w-60 mx-auto"
+        />
+        <div className="w-full flex flex-col items-center justify-center p-10 gap-4">
+          <div
+            className={`${styles.heroHeadline} flex flex-col gap-1 p-3 text-7xl text-center`}
+          >
+            <div>We're all</div>
+            <div>SET to...</div>
+            <div className="text-xl">Spark. Elevate. Transform!</div>
+          </div>
+          <a href="#sections">
+            <Button
+              label="Get Started Now"
+              severity="secondary"
+              size="large"
+              className="min-w-fit uppercase"
+              outlined
+            />
+          </a>
         </div>
-        <a href="#sections">
-          <Button
-            label="Get Started Now"
-            severity="secondary"
-            size="large"
-            className="min-w-fit uppercase"
-            outlined
-          />
-        </a>
+        <h1 className="text-xl text-center">connect.theprofactor@gmail.com</h1>
       </div>
-      <h1 className="text-xl text-center">connect.theprofactor@gmail.com</h1>
     </div>
   );
 };
